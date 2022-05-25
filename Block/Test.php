e237a77ebbaaee74d32e61fb8f789af105dd446c
Omprakash Thakur <iewd1285@gmail.com>
2022-05-25 11:44:28 +0530

    creating a new module

---
+++ Block/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace M2Developer\Knockout\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Test extends Template
+{
+    public function getText()
+    {
+        return "Hello World";
+    }
+}
